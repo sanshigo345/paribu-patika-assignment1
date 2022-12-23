1d9f67f3a519e36a71da908aadad8242d551cba5
Buğra Ercan <38170967+sanshigo345@users.noreply.github.com>
2022-12-23 23:41:49 +0300

    added comments

--- Counter.sol
+++ Counter.sol
@@ -1,17 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-contract Counter {
+contract Counter { 
+    // state variable count, to increase and decrease
     uint public count;
-
+    
+    // standard get function
     function get() public view returns (uint) {
         return count;
     }
-
+    
+    // function to increase the count by 1
     function inc() public {
         count++;
     }
-
+    
+    // function to decrease the count by 1
     function dec() public {
         count--;
     }
